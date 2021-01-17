b296c3702589abfcbc2409633df7c32990628106
muraadkov <46023437+muraadkov@users.noreply.github.com>
2021-01-17 23:36:52 +0600

    GrpcChat

---
+++ ChatClient/ChatClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using ChatService.Protos;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            var username = Console.ReadLine();
+            List<string> history = new List<string>();
+            var channel = GrpcChannel.ForAddress("http://localhost:5001");
+            var client = new ChatRoomService.ChatRoomServiceClient(channel);
+
+            using (var chat = client.Join())
+            {
+                _ = Task.Run(async () =>
+                {
+                    while (await chat.ResponseStream.MoveNext())
+                    {
+                        var response = chat.ResponseStream.Current;
+                        Console.WriteLine($"{response.User} : {response.Text}");
+                    }
+                });
+                foreach(var hist in history)
+                {
+                    Console.WriteLine(hist + "\n");
+                }
+
+                await chat.RequestStream.WriteAsync(new Message { User = username, Text = $"{username} has joined the chat!" });
+
+                string line;
+
+                while ((line = Console.ReadLine()) != null)
+                {
+
+                    if (line.ToUpper() == "EXIT")
+                    {
+                        break;
+                    }
+                    history.Add(line);
+                    await chat.RequestStream.WriteAsync(new Message { User = username, Text = line });
+                }
+
+                await chat.RequestStream.CompleteAsync();
+            }
+
+            Console.WriteLine("Disconnection started!");
+            await channel.ShutdownAsync();
+        }
+    }
+}
---
+++ ChatService/ChatService/Services/ChatServicee.cs
@@ -0,0 +1,31 @@
+﻿using ChatService.Protos;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatService.Services
+{
+    public class ChatServicee : ChatRoomService.ChatRoomServiceBase
+    {
+        private readonly ChatRoom _chatRoom;
+
+        public ChatServicee(ChatRoom chatRoom)
+        {
+            _chatRoom = chatRoom;
+        }
+
+        public override async Task Join(IAsyncStreamReader<Message> requestStream, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+        {
+            if (!await requestStream.MoveNext()) return;
+            do
+            {
+                _chatRoom.Join(requestStream.Current.User, responseStream);
+                await _chatRoom.BroadcastMessageAsync(requestStream.Current);
+            } while (await requestStream.MoveNext());
+                _chatRoom.Remove(context.Peer);
+            
+        }
+    }
+}
